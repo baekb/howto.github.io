4125d6da8a7ef88b042b0a0208f6a1c0cfec9ebc
unknown <tempo9134@naver.com>
2019-12-12 20:45:01 +0900

    hello

---
+++ bluebird.js
@@ -0,0 +1,13 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// stage content:
+(lib.bluebird = function() {
+	this.initialize();
+
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,0,0);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
